10349013a034b6c369a23b218c56af9e98ecca0c
Maycon Ghizzi <maycon.ghizzi@gmail.com>
2013-04-04 22:55:14 -0400

    add list method

--- UsersControl.py
+++ UsersControl.py
@@ -1,15 +1,27 @@
 import sys
 from User import User
 
+users = []
+
 def echo(s):
 	sys.stdout.write(s+'\n')
 
 echo("welcome to UsersControl")
 
 def switch(opt):
+	global users
+	
 	if opt == 'add':
 		user = User(raw_input("username: "),raw_input("email: "));
+		users.append(user)
 		switch(raw_input("\n>"));
+	
+	elif opt == 'list':
+		for i in range(len(users)):
+			user = users[i]
+			echo(str(user.id) + ' | ' + user.name + ' | ' + user.email)
+		switch(raw_input("\n>"));	
+		
 	else:
 		switch(raw_input("WTF your say? do U realy this is a correctly? say help noob\n>"))
 	
